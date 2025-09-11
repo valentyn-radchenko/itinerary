58994648bfcf8a6e667e44905ff0661ff2e6ce10
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2025-09-11 11:23:39 +0200

    Initial commit

---
+++ src/main/java/org/mohyla/itinerary/ItineraryApplication.java
@@ -0,0 +1,13 @@
+package org.mohyla.itinerary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItineraryApplication.class, args);
+    }
+
+}
